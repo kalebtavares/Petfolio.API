d943d7e3064ebd45d80cda776e7f8551407b9ab7
kalebtavares <kalebtechk@gmail.com>
2026-04-22 11:53:04 +0000

    chore: update assembly informational versions and cache files for Petfolio projects

--- Petfolio/Petfolio.Communication/obj/Debug/net8.0/Petfolio.Communication.AssemblyInfo.cs
+++ Petfolio/Petfolio.Communication/obj/Debug/net8.0/Petfolio.Communication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Petfolio.Communication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60993a74fbd2526715456c2e4034f5aa204cbb70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3043fea8c3beca2f1c3dfbf5b4a67608512e727")]
 [assembly: System.Reflection.AssemblyProductAttribute("Petfolio.Communication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Petfolio.Communication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
